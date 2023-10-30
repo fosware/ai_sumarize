6f602f0b2442397c0fe034b856cd2a2705ef5e8b
fosa <fosahadal@protonmail.com>
2023-10-30 12:51:51 -0600

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
         <button
           type="button"
-          onClick={() => window.open("https://github.com/fosware")}
+          onClick={() => window.open("https://github.com/fosware/ai_sumarize")}
           className="black_btn"
         >
           GitHub
